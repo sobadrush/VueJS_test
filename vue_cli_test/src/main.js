6a8f5e1887eb3ef63d7c8c7f3c5a695a7a7514de
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-08-14 21:54:20 +0800

    安裝sass-loader

--- vue_cli_test/src/main.js
+++ vue_cli_test/src/main.js
@@ -6,9 +6,11 @@ import router from './router'
 
 import footer from './components/footer/footer.vue'
 import header from './components/header/header.vue'
+import myFooter from './components/myFooter/myFooter.vue'
 
 Vue.component('app-footer', footer)
 Vue.component('app-header', header)
+Vue.component('app-myFooter', myFooter)
 
 Vue.config.productionTip = false
 

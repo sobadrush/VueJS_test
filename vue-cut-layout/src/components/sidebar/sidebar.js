04c2d7a812632254952961ed7fca9c878c1f8522
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-08-14 23:42:20 +0800

    切sidebar header

---
+++ vue-cut-layout/src/components/sidebar/sidebar.js
@@ -0,0 +1,19 @@
+export default {
+  name: 'sidebar',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
--- vue-cut-layout/src/main.js
+++ vue-cut-layout/src/main.js
@@ -4,6 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// import myHeader from './components/myHeader/myHeader'
+
+// Vue.component('myHeader', myHeader)
+
+import header from './components/header/header.vue'
+import sidebar from './components/sidebar/sidebar.vue'
+
+Vue.component('app-header', header)
+Vue.component('app-sidebar', sidebar)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */

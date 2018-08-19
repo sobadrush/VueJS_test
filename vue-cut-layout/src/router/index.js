d77d6b2d58962b3312558f822e6c0a7490a12c8c
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-08-19 11:43:53 +0800

    設定base，相對根路徑

--- vue-cut-layout/src/router/index.js
+++ vue-cut-layout/src/router/index.js
@@ -9,6 +9,7 @@ import myFlot from '@/components/myFlot/myFlot.vue';
 Vue.use(Router)
 
 export default new Router({
+  // base: '/app', // 設定base，相對根路徑 (当你在 HTML5 history 模式下使用 base 选项之后，所有的 to 属性都不需要写 (基路径) 了) 【https://router.vuejs.org/zh/api/】
   // mode:'history',
   mode:'hash',
   routes: [

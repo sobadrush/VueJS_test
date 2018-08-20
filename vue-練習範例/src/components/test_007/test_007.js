190f047e759ef7d6b0320dbb244b029273f21cc2
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-08-20 22:21:37 +0800

    完成r)

--- vue-練習範例/src/components/test_007/test_007.js
+++ vue-練習範例/src/components/test_007/test_007.js
@@ -4,11 +4,19 @@ export default {
   props: [],
   data () {
     return {
-
+      firstName : 'Roger' ,
+      lastName : 'Lo' ,
     }
   },
   computed: {
-
+    fullName : function(){
+      return `${this.firstName}  ${this.lastName}`;
+    },
+    shortName : function(){
+      let symbol1 = this.firstName.substring(0,1);
+      let symbol2 = this.lastName.substring(0,1);
+      return `${symbol1} . ${symbol2}`;
+    },
   },
   mounted () {
 

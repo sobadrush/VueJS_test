65bb6a42384d996344b8b5521193d560dcf718c3
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-08-21 20:22:59 +0800

    建立完成平級組件溝通AB

--- vue-練習範例/src/components/test_006/test_006.js
+++ vue-練習範例/src/components/test_006/test_006.js
@@ -1,8 +1,47 @@
+import Vue from 'vue';
+
+Vue.component('childA'/*html中要改串燒式*/, {
+  template : `<div class="my-border-blue my-bg-color-A margin-around">
+                <h1 @click.prevent="sayHi(empName)">Child-A , #{empName}</h1>
+              </div>`,
+  data() {
+    return {
+      empName : '高進'
+    }
+  },
+  delimiters: ['#{','}'],
+  props: [],
+  methods: {
+    sayHi (_name) {
+      alert('Hi, ' + _name);
+    }
+  }
+});
+
+
+Vue.component('childB'/*html中要改串燒式*/, {
+  template : `<div class="my-border-blue my-bg-color-B margin-around">
+                <h1 @click.prevent="sayHi(empName)">Child-B , #{empName}</h1>
+              </div>`,
+  data() {
+    return {
+      empName : '仇笑癡'
+    }
+  },
+  delimiters: ['#{','}'],
+  props: [],
+  methods: {
+    sayHi (_name) {
+      alert('Hi, ' + _name);
+    }
+  }
+});
+
 export default {
   name: 'test-006',
   components: {},
   props: [],
-  data () {
+  data() {
     return {
 
     }
@@ -10,7 +49,7 @@ export default {
   computed: {
 
   },
-  mounted () {
+  mounted() {
 
   },
   methods: {

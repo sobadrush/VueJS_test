ba8b23c9534306dd3c6c63bc03cf154e281c91c8
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-08-24 19:35:19 +0800

    JS原生達到雙向

--- vue-練習範例/src/components/test_002/test_002.js
+++ vue-練習範例/src/components/test_002/test_002.js
@@ -2,18 +2,33 @@ export default {
   name: 'test-002',
   components: {},
   props: [],
-  data () {
+  data() {
     return {
-      myFavoriteGame : {
-        name : '龍珠超宇宙2'
+      myFavoriteGame: {
+        name: '龍珠超宇宙2'
       }
     }
   },
   computed: {
 
   },
-  mounted () {
+  created() {
+    console.log('%c%s' , 'color:red' , '>>>>>>>>>> test_002 Created <<<<<<<<<');
+  },
+  mounted() {
+    console.log('%c%s' , 'color:green' , '>>>>>>>>>> test_002 mounted <<<<<<<<<');
 
+    // 當建立test002元件，有myInput1時，才註冊此事件
+    let myInput1 = document.querySelector('#myInput1');
+    myInput1.addEventListener("keyup", function (e) {
+      console.log('this = ', this);
+      console.log('key in value = ', this.value);
+      let text = this.value;
+
+      let mySpan = $(this).next()[0];
+      console.log('mySpan >>> ', mySpan);
+      mySpan.innerText = text;
+    });
   },
   methods: {
 

38f22c409cb44daf8c4e75e5b36cd2af516da94c
sobadrush <sobadrush@outlook.com>
2018-08-27 15:50:37 +0800

    完成表單控件，textarea

---
+++ vue-練習範例/src/components/practice6/formComp002/formComp002.js
@@ -0,0 +1,19 @@
+export default {
+  name: 'form-comp-002',
+  components: {},
+  props: [],
+  data () {
+    return {
+      myMessage : ''
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
---
+++ vue-練習範例/src/components/practice6/formComp002/formComp002.vue
@@ -0,0 +1,4 @@
+<template src="./formComp002.html"></template>
+<script src="./formComp002.js"></script>
+<style src="./formComp002.scss" scoped lang="scss"></style>
+

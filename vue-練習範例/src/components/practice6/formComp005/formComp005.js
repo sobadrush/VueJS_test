8ca6bdbe3e69ad02f6f48bc87e3fbf8a4b01e91f
sobadrush <sobadrush@outlook.com>
2018-08-27 17:30:31 +0800

    v-for + select option

--- vue-練習範例/src/components/practice6/formComp005/formComp005.js
+++ vue-練習範例/src/components/practice6/formComp005/formComp005.js
@@ -7,6 +7,13 @@ export default {
       selectedData : '',
       selectedData2 : 'defaultGG', // 預設綁訂某個 option 的 value，即可達到初始選擇某值的效果
       selectedData3 : [], // 多選的select option，必須設定 array
+      //----------------------------------------------------------
+      selectedData4: 'Apple', // 預設綁訂某個 option 的 value，即可達到初始選擇某值的效果
+      myOptions: [
+        { text: 'One'  , value: 'Apple' },
+        { text: 'Two'  , value: 'Book'  },
+        { text: 'Three', value: 'Cat'   }
+      ],
     }
   },
   computed: {

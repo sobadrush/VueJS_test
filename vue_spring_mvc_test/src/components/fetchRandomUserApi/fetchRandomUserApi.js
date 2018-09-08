12a764d0b4441f1cbd264b413c1d005336b1c234
TizzyBac <TizzyBac TizzyBac@icloud.com>
2018-09-08 21:23:48 +0800

    完成axios+computed分頁

--- vue_spring_mvc_test/src/components/fetchRandomUserApi/fetchRandomUserApi.js
+++ vue_spring_mvc_test/src/components/fetchRandomUserApi/fetchRandomUserApi.js
@@ -6,22 +6,39 @@ export default {
   data() {
     return {
       queryNumber: +10,/*查詢筆數*/
+      countOfPage: +5,/*每頁顯示筆數*/
+      currPage: 1,
       empList: []
     }
   },
   computed: {
-
+    pageStart: function () {
+      return (this.currPage - 1) * this.countOfPage;
+    },
+    totalPage: function () {
+      return Math.ceil(this.empList.length / this.countOfPage);
+    }
   },
   mounted() {
 
   },
+  filters: {
+    upperCaseCharByNum(_data, _num) {
+      // 【根據_num，決定哪個char要改大寫】
+      console.log(`======= filter : upperCaseCharByNum() , ${_data} , ${_num} =======`);
+      let targetCharUpper = _data.charAt(_num).toUpperCase();
+      // console.log(_data.substr(0,_num) , targetCharUpper , _data.substr(_num + 1, _data.length));
+      return _data.substr(0,_num) + targetCharUpper + _data.substr(_num + 1, _data.length);
+    }
+  },
   methods: {
-    clearData(){
-      console.log(`%c%s`,'colorblue','=== clearData ===');
-      this.empList = [];
+    clearData() {
+      console.log(`%c%s`, 'colorblue', '=== clearData ===');
+      // this.empList = [];
+      this.empList.splice(0, this.empList.length); // 清空陣列
     },
-    doFetchData( _queryNumber ) {
-      console.log(`%c%s`, 'color:red' , '============ doFetchData ===========');
+    doFetchData(_queryNumber) {
+      console.log(`%c%s`, 'color:red', '============ doFetchData ===========');
 
       // let toUrl = `https://randomuser.me/api/?results=100`; // results: 測試資料API的查詢筆數
       // this.$axios.get(toUrl)
@@ -46,6 +63,12 @@ export default {
         .catch((err) => {
           console.log(err);
         });
-    }
+    },
+    setPage(idx) {
+      if( idx <= 0 || idx > this.totalPage ){
+        return;
+      }
+      this.currPage = idx;
+    },
   }
 }
